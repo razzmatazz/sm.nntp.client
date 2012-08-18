3eb67dddf76755006330ccae61c87488a2b28445
Saulius Menkevicius <saulius.menkevicius@gmail.com>
2012-08-18 14:24:39 +0300

    NntpServerError: server-specific nntp error exception class

---
+++ src/sm/nntp/client/NntpServerError.java
@@ -0,0 +1,10 @@
+package sm.nntp.client;
+
+public class NntpServerError extends NntpConnectionError {
+	private static final long serialVersionUID = 1L;
+
+	public NntpServerError(String string) {
+		super(string);
+	}
+	
+}
