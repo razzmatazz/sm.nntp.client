f5ca9fb0ecf4893684113373657cf89ccb260b7b
Saulius Menkevicius <saulius.menkevicius@gmail.com>
2012-08-18 15:51:00 +0300

    NntpConnection: remove unimplemented methods, as we don't know what is the best api strategy for those now

--- src/sm/nntp/client/NntpConnection.java
+++ src/sm/nntp/client/NntpConnection.java
@@ -12,16 +12,4 @@ public interface NntpConnection extends Closeable {
 	public Iterable<NewsgroupStatus> fetchListOfNewsgroupsPublishedSince(
 			Date since) throws IOException;
 
-	public Iterable<ArticleHeader> fetchArticleHeadersForNewsgroup(
-			String newsgroup) throws IOException;
-
-	public Iterable<String> fetchMessageIdsForArticlesInNewsgroupPostedSince(
-			String newsgroup, Date since) throws IOException;
-
-	public Iterable<String> fetchMessageIdsForArticlesPostedSince(Date since) throws IOException;
-
-	public ArticleHeader fetchArticleHeaderForMessageId(String messageId) throws IOException;
-
-	public Article fetchArticleByMessageId(String messageId) throws IOException;
-
 }
--- src/sm/nntp/client/internal/NntpConnectionImplementation.java
+++ src/sm/nntp/client/internal/NntpConnectionImplementation.java
@@ -1,11 +1,10 @@
 package sm.nntp.client.internal;
 
 import java.io.IOException;
+
 import java.net.Socket;
 import java.util.Date;
 
-import sm.nntp.client.Article;
-import sm.nntp.client.ArticleHeader;
 import sm.nntp.client.NewsgroupStatus;
 import sm.nntp.client.NntpConnection;
 import sm.nntp.client.NntpStreamInspector;
@@ -71,47 +70,5 @@ public class NntpConnectionImplementation implements NntpConnection {
 	public Iterable<NewsgroupStatus> fetchListOfNewsgroupsPublishedSince(Date since) throws IOException {
 		ensureSetupDone();		
 		return new ListNewNewsgroupsCommand(since).executeOn(cmdStream);
-	}
-	
-	/* (non-Javadoc)
-	 * @see sm.nntp.client.NntpConnection#fetchArticleHeadersForNewsgroup(java.lang.String)
-	 */
-	@Override
-	public Iterable<ArticleHeader> fetchArticleHeadersForNewsgroup(String newsgroup) {
-		throw new UnsupportedOperationException();
-	}
-	
-	/* (non-Javadoc)
-	 * @see sm.nntp.client.NntpConnection#fetchMessageIdsForArticlesInNewsgroupPostedSince(java.lang.String, java.util.Date)
-	 */
-	@Override
-	public Iterable<String> fetchMessageIdsForArticlesInNewsgroupPostedSince(
-			String newsgroup,
-			Date since) {
-		throw new UnsupportedOperationException();
-	}
-	
-	/* (non-Javadoc)
-	 * @see sm.nntp.client.NntpConnection#fetchMessageIdsForArticlesPostedSince(java.util.Date)
-	 */
-	@Override
-	public Iterable<String> fetchMessageIdsForArticlesPostedSince(Date since) {
-		throw new UnsupportedOperationException();
-	}
-		
-	/* (non-Javadoc)
-	 * @see sm.nntp.client.NntpConnection#fetchArticleHeaderForMessageId(java.lang.String)
-	 */
-	@Override
-	public ArticleHeader fetchArticleHeaderForMessageId(String messageId) {
-		throw new UnsupportedOperationException();
-	}
-	
-	/* (non-Javadoc)
-	 * @see sm.nntp.client.NntpConnection#fetchArticleByMessageId(java.lang.String)
-	 */
-	@Override
-	public Article fetchArticleByMessageId(String messageId) {
-		throw new UnsupportedOperationException();
 	}
 }
